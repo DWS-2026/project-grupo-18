a40f4ec358338c2a224ec60974e37b42c904cb25
qixfnqu <73741157+qixfnqu@users.noreply.github.com>
2026-02-13 12:05:39 +0100

    Updated css, make dynamic header and footer

--- src/main/java/com/example/cybercert/CyberController.java
+++ src/main/java/com/example/cybercert/CyberController.java
@@ -3,46 +3,55 @@ package com.example.cybercert;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CyberController {
 
     @GetMapping("/")
-    public String index() {
-        return "index"; // Spring busca index.html en templates
+    public String index(Model model) {
+        model.addAttribute("pageCss", "index");
+        return "index"; 
     }
     @GetMapping("/login")
-    public String login() {
-        return "login"; // renderiza login.html
+    public String login(Model model) {
+        model.addAttribute("pageCss", "auth");
+        return "login"; 
     }
 
     @GetMapping("/register")
-    public String register() {
-        return "register"; // renderiza register.html
+    public String register(Model model) {
+        model.addAttribute("pageCss", "auth");
+        return "register"; 
     }
 
     @GetMapping("/admin")
-    public String admin() {
-        return "admin"; // renderiza admin.html
+    public String admin(Model model) {
+        model.addAttribute("pageCss", "admin");
+        return "admin"; 
     }
 
     @GetMapping("/profile")
-    public String profile() {
-        return "profile"; // renderiza profile.html
+    public String profile(Model model) {
+        model.addAttribute("pageCss", "profile");
+        return "profile";
     }
 
     @GetMapping("/certification")
-    public String certification() {
-        return "certification"; // renderiza certification.html
+    public String certification(Model model) {
+        model.addAttribute("pageCss", "certification");
+        return "certification"; 
     }
     @GetMapping("/checkout")
-    public String checkout() {
-        return "checkout"; // renderiza certification.html
+    public String checkout(Model model) {
+        model.addAttribute("pageCss", "checkout");
+        return "checkout"; 
     }
     @GetMapping("/shoppingcart")
-    public String shoppingcart() {
-        return "shopping-cart"; // renderiza certification.html
+    public String shoppingcart(Model model) {
+        model.addAttribute("pageCss", "shoping-cart");
+        return "shopping-cart"; 
     }
 }
 
