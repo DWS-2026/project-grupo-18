d0cf22d9ab56f9decefbcb78850002db9556969d
rikydefco20 <rikydefco2006@gmail.com>
2026-02-13 12:15:28 +0100

    Creation of the BBDD

--- src/main/java/com/example/cybercert/CyberController.java
+++ src/main/java/com/example/cybercert/CyberController.java
@@ -1,26 +1,43 @@
 package com.example.cybercert;
 
 
+import org.springframework.ui.Model;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CyberController {
+    @Autowired
+    private UserRepository userRepository;
+
 
     @GetMapping("/")
     public String index() {
         return "index"; // Spring busca index.html en templates
     }
     @GetMapping("/login")
-    public String login() {
-        return "login"; // renderiza login.html
-    }
+public String login() {
+    return "login";
+}
 
-    @GetMapping("/register")
-    public String register() {
-        return "register"; // renderiza register.html
-    }
+@GetMapping("/register")
+public String showRegisterForm(Model model) {
+    model.addAttribute("user", new User());
+    return "register";
+}
+
+@PostMapping("/register")
+public String registerUser(@ModelAttribute User user) {
+
+    userRepository.save(user);
+
+    return "redirect:/login";
+}
+
 
     @GetMapping("/admin")
     public String admin() {
