9ae458cd6c63372fd74fd309439e58f5d26eca91
k1di3 <k1di3@kali.Kali>
2026-03-15 11:57:06 +0100

    Solved the problem with csrf tokens and with duplicate email registration

--- src/main/java/com/example/cybercert/UserRepository.java
+++ src/main/java/com/example/cybercert/UserRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
         Optional<User> findByUsername(String username);
+        Optional<User> findByEmail(String email);
         void deleteByUsername(String username);
 
 }
