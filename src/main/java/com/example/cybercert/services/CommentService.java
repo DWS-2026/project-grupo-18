994a342844baf7d4bb6830f3a363e2f5427d3c17
rikydefco20 <rikydefco2006@gmail.com>
2026-05-05 09:23:26 +0200

    Page fix

--- src/main/java/com/example/cybercert/services/CommentService.java
+++ src/main/java/com/example/cybercert/services/CommentService.java
@@ -1,6 +1,8 @@
-package com.example.cybercert.services;
+﻿package com.example.cybercert.services;
 
 import org.springframework.stereotype.Service;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import com.example.cybercert.models.Certification;
 import com.example.cybercert.models.Comment;
@@ -21,6 +23,10 @@ public class CommentService {
         return commentsRepository.findAll();
     }
 
+    public Page<Comment> findAll(Pageable pageable) {
+        return commentsRepository.findAll(pageable);
+    }
+
     public Optional<Comment> findById(Long id) {
         return commentsRepository.findById(id);
     }
@@ -35,6 +41,10 @@ public class CommentService {
 
     public List<Comment> getCommentsByCertification(Long certificationId) {
         return commentsRepository.findByCertificationIdOrderByCreatedAtDesc(certificationId);
+    }
+
+    public Page<Comment> getCommentsByCertification(Long certificationId, Pageable pageable) {
+        return commentsRepository.findByCertificationIdOrderByCreatedAtDesc(certificationId, pageable);
     }
 
     public Comment addComment(User user, Certification certification, String text, int rating) {
